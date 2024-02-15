7402bdb4ea71b78edc4d0b43032b76c2b8497b06
tlsgkdns <tlsgkdns@gmail.com>
2024-02-15 17:20:12 +0900

    Feat: redisTemplate 약간 수정

---
+++ src/main/kotlin/com/ressul/ressul/infra/RedisConfig.kt
@@ -0,0 +1,29 @@
+package com.ressul.ressul.infra
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+    @Value("\${spring.redis.host}")
+    lateinit var host: String
+    @Value("\${spring.redis.port}")
+    var port: Int = 0
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory(
+        RedisStandaloneConfiguration(host, port)
+    )
+
+    @Bean
+    fun redisTemplate() = RedisTemplate<Any, Any>().apply{
+            this.connectionFactory = redisConnectionFactory()
+            this.keySerializer = StringRedisSerializer()
+            this.valueSerializer = StringRedisSerializer()
+        }
+}
