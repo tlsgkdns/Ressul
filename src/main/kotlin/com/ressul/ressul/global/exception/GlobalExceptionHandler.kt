2efbbd2e232199a90436c83305b2f3513f073824
moveuk <v_donguk@naver.com>
2024-02-15 09:18:47 +0900

    Feat: 예외 처리 기본 형태 정리

---
+++ src/main/kotlin/com/ressul/ressul/global/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,18 @@
+package com.ressul.ressul.global.exception
+
+import com.ressul.ressul.global.exception.dto.ErrorResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(ModelNotFoundException::class)
+    fun handleModelNotFoundException(e: ModelNotFoundException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse(e.message))
+    }
+
+}
---
+++ src/main/kotlin/com/ressul/ressul/global/exception/dto/ErrorResponse.kt
@@ -0,0 +1,3 @@
+package com.ressul.ressul.global.exception.dto
+
+data class ErrorResponse(val message: String?)
