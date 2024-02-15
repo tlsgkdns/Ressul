a4c804d8670566820a0c8e98bff57148f6fae369
tlsgkdns <tlsgkdns@gmail.com>
2024-02-15 20:15:35 +0900

    필요없는 import 삭제

--- src/main/kotlin/com/ressul/ressul/RessulApplication.kt
+++ src/main/kotlin/com/ressul/ressul/RessulApplication.kt
@@ -3,7 +3,6 @@ package com.ressul.ressul
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
 @EnableJpaAuditing
