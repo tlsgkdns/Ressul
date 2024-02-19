d12be279cdea1bf3761070d394ccf72ecb2ef0a9
Myolang <myohanmyolang@gmail.com>
2024-02-20 05:55:37 +0900

    Feat : 최근 인기 이력서 Service

--- src/main/kotlin/com/ressul/ressul/infra/redis/resume/ResumeRedisService.kt
+++ src/main/kotlin/com/ressul/ressul/infra/redis/resume/ResumeRedisService.kt
@@ -13,66 +13,85 @@ open class ResumeRedisService(
 ) {
 	private val POPULAR_RESUME_KEY = "PopularResume"
 	private val RECENTLY_RESUME_VIEWS_KEY = "RecentlyResumeViews"
-	private val UPDATED_VIEWS_KEY = "UpdatedViews"
+	private val UPDATED_VIEWS_KEY = "ResumeUpdatedViews"
+	private val UPDATE_COUNT_KEY = "ResumeUpdateCount"
 	private val VIEW_COUNT = 100
 
 	private val popularResumeNumber = 10L
 
-	private fun isViewsUpdate() =
-		redisTemplate.opsForValue().get(UPDATED_VIEWS_KEY)!! > VIEW_COUNT
+
+	private fun shouldViewsUpdate() =
+		redisTemplate.opsForValue().run {
+			takeIf { get(UPDATE_COUNT_KEY)!! > VIEW_COUNT }
+				?.let {
+					redisTemplate.opsForValue().set(UPDATE_COUNT_KEY, 0)
+					true
+				}
+				?: false
+		}
+
 
 	private fun updateViews() {
-		redisTemplate.opsForHash<Long, Int>()
-			.let {  }
+		redisTemplate.opsForHash<Int, Int>().run {
+			val recentlyViews = entries(RECENTLY_RESUME_VIEWS_KEY)
+			val updatedViews = entries(UPDATED_VIEWS_KEY)
+
+			recentlyViews.keys.forEach { key ->
+				takeIf { updatedViews[key] != null }
+					?.let { updatedViews[key] = recentlyViews[key]?.minus(updatedViews[key]!!) }
+					?: let { updatedViews[key] = recentlyViews[key] }
+			}
+		}
 	}
 
 	private fun getUpdatedViews() =
-		redisTemplate.opsForHash<Int?, Int?>().entries(UPDATED_VIEWS_KEY)
-
+		redisTemplate.opsForHash<Int, Int>().entries(UPDATED_VIEWS_KEY)
 
 	private fun getRecentlyViews() =
-		redisTemplate.opsForHash<Int?, Int?>().entries(RECENTLY_RESUME_VIEWS_KEY)
+		redisTemplate.opsForHash<Int, Int>().entries(RECENTLY_RESUME_VIEWS_KEY)
 
 	@Scheduled(fixedRate = 10 * 60 * 1000)
 	private fun setPopularResume() {
-
 		getRecentlyViews()
 			.also { redisTemplate.delete(RECENTLY_RESUME_VIEWS_KEY) }
+			.also { redisTemplate.delete(POPULAR_RESUME_KEY) }
 			.also { recentlyViews ->
 				recentlyViews.toList()
 					.run { sortedBy { it.second } }
 					.subList(0, popularResumeNumber.toInt())
 					.run { map { it.first } }
 					.also { redisTemplate.opsForList().rightPushAll(POPULAR_RESUME_KEY, it) }
-
 			}
 			.run { updateViewsWith(this) }
 	}
 
 	private fun updateViewsWith(recentlyViews: Map<Int, Int>) {
 		val updatedViews = getUpdatedViews()
-		recentlyViews.run { // TODO : 이미 처리가 끝난 조회수를 뺀 이후 최종적으로 업데이트 시킨다.
+		recentlyViews.run { // NOTE : 이미 처리가 끝난 조회수를 뺀 이후 최종적으로 업데이트 시킨다.
 			keys.map { it.toLong() }
 				.let { resumeRepository.findAllById(it) }
-				.onEach { it.views = (((this[it.id!!.toInt()]?.minus(updatedViews[it.id!!.toInt()]!!))?.toLong()!!)) }
+				.onEach {
+					it.views +=
+						this[it.id!!.toInt()]!!.toLong() - updatedViews[it.id!!.toInt()]!!.toLong()
+				}
 				.let { resumeRepository.saveAllAndFlush(it) }
 		}
 	}
 
 
 	fun addView(resumeId: Long) =
-		redisTemplate.opsForHash<Long?, Int?>().run {
+		redisTemplate.opsForHash<Long, Int>().run {
 			get(RECENTLY_RESUME_VIEWS_KEY, resumeId)
 				?.let { put(RECENTLY_RESUME_VIEWS_KEY, resumeId, it + 1) }
 				?: put(RECENTLY_RESUME_VIEWS_KEY, resumeId, 1)
 
-			redisTemplate.opsForValue().increment(UPDATED_VIEWS_KEY)
+			redisTemplate.opsForValue().increment(UPDATE_COUNT_KEY)
 
-			if (isViewsUpdate())
+			if (shouldViewsUpdate())
 				runBlocking(Dispatchers.IO) { async { updateViews() } }
 		}
 
 	fun getPopularResume() =
-		redisTemplate.opsForList().range(POPULAR_RESUME_KEY, 0, popularResumeNumber)?.toList()
+		redisTemplate.opsForList().range(POPULAR_RESUME_KEY, 0, popularResumeNumber)?.toList()?.map { it.toLong() }
 			?: emptyList()
 }
