9d517d145351a44325825c6fba4bedae7df9a35a
moveuk <v_donguk@naver.com>
2024-02-19 20:16:04 +0900

    Test: Test Profile 작성 및 사용

---
+++ src/test/kotlin/com/ressul/ressul/common/BaseRepositoryTest.kt
@@ -0,0 +1,10 @@
+package com.ressul.ressul.common
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BaseRepositoryTest
