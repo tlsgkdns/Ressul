93d8db9612d6fd2ff989a8268297e986a461cbea
Myolang <myohanmyolang@gmail.com>
2024-02-21 15:25:18 +0900

    Refactor : QueryDsl 최근 1만건 이내만 검색하도록 조건을 추가하여 검색 속도 올리기
    TODO : 검색 조건에 따라 1만건 이후로도 검색 할 수 있도록 변경하기

--- src/main/kotlin/com/ressul/ressul/domain/resume/repository/ResumeQueryDslRepository.kt
+++ src/main/kotlin/com/ressul/ressul/domain/resume/repository/ResumeQueryDslRepository.kt
@@ -3,6 +3,7 @@ package com.ressul.ressul.domain.resume.repository
 import com.querydsl.core.BooleanBuilder
 import com.ressul.ressul.domain.resume.dto.SearchResumeRequest
 import com.ressul.ressul.domain.resume.model.QResumeEntity
+import com.ressul.ressul.domain.resume.model.ResumeEntity
 import com.ressul.ressul.infra.querydsl.QueryDslSupport
 import org.springframework.stereotype.Repository
 
@@ -11,21 +12,39 @@ class ResumeQueryDslRepository : QueryDslSupport() {
 
 	private val resume = QResumeEntity.resumeEntity
 
+	private fun getTableRow() =
+		queryFactory.selectFrom(resume).orderBy(resume.id.desc()).limit(1).fetch()?.get(0)?.id
 
-	fun searchAndPaginationBy(dto: SearchResumeRequest, keyword: String, page: Int, size: Int) =
+
+	fun searchAndPaginationBy(dto: SearchResumeRequest, keyword: String, page: Int, size: Int): List<ResumeEntity> =
 		queryFactory
 			.selectFrom(resume)
 			.where(
 				BooleanBuilder()
-					.or(dto.title?.let { resume.title.contains(keyword) })
-					.or(dto.education?.let { resume.education.contains(keyword) })
-					.or(dto.introduction?.let { resume.introduction.contains(keyword) })
-					.or(dto.certification?.let { resume.certification.contains(keyword) })
+					.or(dto.title?.let { containsTitle(keyword) })
+					.or(dto.education?.let { containsEducation(keyword) })
+					.or(dto.introduction?.let { containsIntroduction(keyword) })
+					.or(dto.certification?.let { containsCertification(keyword) })
+					.and(getTableRow()?.let { getGt(it) })
 			)
 			.orderBy(resume.id.desc())
 			.limit(size.toLong())
 			.offset(((page - 1) * size).toLong())
 			.fetch()
 
+	private fun containsEducation(keyword: String) =
+		resume.education.contains(keyword)
 
+	private fun containsCertification(keyword: String) =
+		resume.certification.contains(keyword)
+
+	private fun containsTitle(keyword: String) =
+		resume.title.contains(keyword)
+
+	private fun containsIntroduction(keyword: String) =
+		resume.introduction.contains(keyword)
+
+	fun getGt(tableRow: Long) =
+		takeIf { tableRow > 100000L }
+			?.let { resume.id.gt(tableRow - 100000L) }
 }
